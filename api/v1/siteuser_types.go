dfbe335f66c0ca9af29b9413ce8518b39e3da78e
VyogoDev <dev@vyogo.tech>
2026-06-30 10:36:43 +1000

    refactor: replace scaffolding Foo placeholders with Description spec field across all CRDs

--- api/v1/siteuser_types.go
+++ api/v1/siteuser_types.go
@@ -28,8 +28,8 @@ type SiteUserSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of SiteUser. Edit siteuser_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Description provides a human-readable explanation of the SiteUser.
+	Description string `json:"description,omitempty"`
 }
 
 // SiteUserStatus defines the observed state of SiteUser
