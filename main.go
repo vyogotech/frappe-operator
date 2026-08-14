9f8b09afca4d665d60d9681016072c5b81733f89
VyogoDev <dev@vyogo.tech>
2026-08-14 12:07:52 +1000

    chore: gofmt import ordering in main.go

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/rest"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	crcfg "sigs.k8s.io/controller-runtime/pkg/config"
